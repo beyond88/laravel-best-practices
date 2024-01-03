224c45f40f73862440c7a067fb8c90be7a4e3dbd
beyond88 <muhin.cse.diu@gmail.com>
2024-01-03 16:04:40 +0600

    fix: migration error

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Offer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'price',
+        'description',
+        'author_id',
+        'status'
+    ];
 }
--- database/factories/OfferFactory.php
+++ database/factories/OfferFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Constants\Status;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Offer>
@@ -17,7 +18,11 @@ class OfferFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(3),
+            'price' => fake()->numberBetween(100, 1000),
+            'status' => Status::DRAFT,
+            'author_id' => User::factory()
         ];
     }
 }
